a4a0092096766afbd70a6116354d8ecd6c068bff
minichichi <minichi6985@gmail.com>
2023-05-02 17:05:43 +0900

    test2

--- server.py
+++ server.py
@@ -6,6 +6,6 @@ app  = Flask(__name__)
 
 @app.route('/')
 def index():
-    return str(random.random())
+    return 'random : <strong>'+str(random.random())+'<strong>' # 랜덤한 값 출력
 
 app.run(debug=True) ##debug 모드로 플라스크 실행(플라스크가 꺼졌다가 바로 실행) --> 실제 서비스일 땐 X
